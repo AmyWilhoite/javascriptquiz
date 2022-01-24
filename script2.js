7adf27a14bb38014f8329540abcbe1a334949f4a
AmyWilhoite <amywilhoite@gmail.com>
2022-01-24 18:11:52 -0500

    trying out different functions to see what works well wtih objects

--- script2.js
+++ script2.js
@@ -1,41 +1,68 @@
 console.log("this page works!!")
 
 // define variables for display on HTML using Queryselector
-var questionEl = document.querySelectorAll("getQuestion");
+var questionEl = document.querySelector("#getQuestion");
+var answer1El = document.querySelector("btn1");
+var answer2El = document.querySelector("btn2");
+var answer3El = document.querySelector("btn2");
+var answer4El = document.querySelector("btn4");
 var timerEl = document.getElementById("timer");
+var scoreEl = document.querySelector("#score");
+var mainContainerEl = document.querySelector("#main");
 
 
-
-// tried creating construction function using objects
+// tried creating construction function using objects easily to create more questions if needed
 function QuestionFactory (questions, answers, correctAns){
     this.question = questions;
     this.answer = answers;
     this.correctAns = correctAns;
-    // i can create a method/function to return correct answer
-    // document.getElementById(checkAnswer).onclick = function(){need to enter code to check}
- 
 }
+    // i can create a method/function to insert here return correct answer
+    // this.getElementById(checkAnswer).onclick = function()){
+        // if (correctAns === true){ } }
 
-// created a function for each question using the construction model
+
+// created objects that contain all the data for each question using the construction model
+
 var question1 = new QuestionFactory("An API interacts with programs in:", ["browsers", "scripts", "platforms", "all of the above"], "browser");
-console.log(question1);
+console.log(question1.question);
+console.log(question1.answers);
+console.log(question1.correctAns);
 
 var question2 = new QuestionFactory("Which of the following is a string?", ["cat", "25", "true or false", "Null"], "cat");
-console.log(question2)
-document.getElementById("getQuestion").innerHTML= "Question:" + question2.questions;
 
 var question3 = new QuestionFactory("Which of the following is a loop:", ["While", "Click", "Return", "All of the above"], "While" );
-console.log(question3)
-document.getElementById("getQuestion").innerHTML= "Question:" + question3.questions;
 
 var question4 = new QuestionFactory("WWOWOW", ["While", "Click", "Return", "All of the above"], "While", "Answer Correct" );
 console.log(question4)
-document.getElementById("getQuestion").innerHTML= "Question:" + question4.questions;
+
+// get question 1 and array of answers to 1 to show up on screen
+// use myJSON to stringify some of this so we can retrieve object
+questionEl.textContent = "Question: " + question1.question;
+answer1El.textContent = question1.answers[0];
 
 
 
-// Timer Section
-// need to set start time 
+// create event listener to listen for user click and check to see if it matches correctAns
+// var numberofAnswers = document.querySelectorAll(".bstyle").length;
+// var userInput ="";
+// var scoreStart = 0;
+// var updateScore = 
+
+// for (var i =0; i < numberofAnswers; i++) {
+// document.querySelectorAll(".bstyle")[i].addEventListener("click", function(){
+//      if (userInput === correctAns);
+//     // updateScore = scoreStart + i + 5
+//     scoreEl.textContent = "test" 
+//     });
+
+// }
+// if answer is correct, increase score by 5 points else decrease timer time by 5 seconds
+
+
+
+// Timer Section --- this is working just need to make sure it deducts time when answer is incorrect
+// start time 
 var secondsLeft = 30;
 
 // function to get my timer to count down
@@ -44,15 +71,15 @@ var timerInterval = setInterval(function() {
     timerEl.textContent = secondsLeft + " Seconds Remaining";
 
     if(secondsLeft === 0) {
-      // Stops execution of action at set interval
+      // Stops set interval
       clearInterval(timerInterval);
-      // Calls function to message user game over
+      // message user game over
       timerEl.textContent = "GAME OVER!"
     }
 }, 1000);
 
 
 // deductions by 5 seconds if incorrect 
+// document.getElementById(id).onclick = function(){code}
 
-// score
-// score display high score
+// score i believe local storage / log high score.
