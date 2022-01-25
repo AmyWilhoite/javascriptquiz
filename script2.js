debda0370ba8813786f24e3503a49e7c528c6ab8
AmyWilhoite <amywilhoite@gmail.com>
2022-01-25 00:44:23 -0500

    latest version

--- script2.js
+++ script2.js
@@ -1,20 +1,15 @@
 console.log("this page works!!")
 
 // define variables for display on HTML using Queryselector
-var questionEl = document.querySelector("#getQuestion");
-var answer1El = document.querySelector("btn1");
-var answer2El = document.querySelector("btn2");
-var answer3El = document.querySelector("btn2");
-var answer4El = document.querySelector("btn4");
 var timerEl = document.getElementById("timer");
-var scoreEl = document.querySelector("#score");
-var mainContainerEl = document.querySelector("#main");
-
+var scoreEl = document.querySelector("score");
+var questionEl = document.querySelector("getQuestion");
+var answer1El = document.querySelector("getAnswer");
 
 // tried creating construction function using objects easily to create more questions if needed
 function QuestionFactory (questions, answers, correctAns){
-    this.question = questions;
-    this.answer = answers;
+    this.questions = questions;
+    this.answers = answers;
     this.correctAns = correctAns;
 }
     // i can create a method/function to insert here return correct answer
@@ -28,31 +23,43 @@ var question1 = new QuestionFactory("An API interacts with programs in:", ["brow
 console.log(question1.question);
 console.log(question1.answers);
 console.log(question1.correctAns);
-
 var question2 = new QuestionFactory("Which of the following is a string?", ["cat", "25", "true or false", "Null"], "cat");
-
 var question3 = new QuestionFactory("Which of the following is a loop:", ["While", "Click", "Return", "All of the above"], "While" );
 
-var question4 = new QuestionFactory("WWOWOW", ["While", "Click", "Return", "All of the above"], "While", "Answer Correct" );
-console.log(question4)
 
-// get question 1 and array of answers to 1 to show up on screen
-// use myJSON to stringify some of this so we can retrieve object
-questionEl.textContent = "Question: " + question1.question;
-answer1El.textContent = question1.answers[0];
+// display questions and answers from constructor function
+function showQuetions(index){
+    const getQuestion = document.querySelector("getQuestion");
+
+    //why are we using span? creating a new span and div tag for question and option and passing the value using array index
+    // let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    // let option_tag = '<div class="option"><span>'+ questions[index].answers[0] +'</span></div>'
+    // + '<div class="option"><span>'+ questions[index].answers[1] +'</span></div>'
+    // + '<div class="option"><span>'+ questions[index].answers[2] +'</span></div>'
+    // + '<div class="option"><span>'+ questions[index].answers[3] +'</span></div>';
+    // getQuestion.innerHTML = que_tag; //adding new span tag inside que_tag
+    // option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+    
+    // const option = option_list.querySelectorAll(".option");
+
+    // // set onclick attribute to all available options
+    // for(i=0; i < option.length; i++){
+    //     option[i].setAttribute("onclick", "optionSelected(this)");
+    // }
+}
 
 
 
 // create event listener to listen for user click and check to see if it matches correctAns
 // var numberofAnswers = document.querySelectorAll(".bstyle").length;
-// var userInput ="";
-// var scoreStart = 0;
-// var updateScore = 
+// var userInput =i;
+// var userScore = 0;
+
 
 // for (var i =0; i < numberofAnswers; i++) {
 // document.querySelectorAll(".bstyle")[i].addEventListener("click", function(){
 //      if (userInput === correctAns);
-//     // updateScore = scoreStart + i + 5
+//     // updateScore = userScore + 5
 //     scoreEl.textContent = "test" 
 //     });
 
@@ -77,6 +84,16 @@ var timerInterval = setInterval(function() {
       timerEl.textContent = "GAME OVER!"
     }
 }, 1000);
+
+// subtract time
+
+// // subtractTime.addEventListener("click", function() {
+//     if (count > 0) {
+//       count--;
+//       counter.textContent = count;
+//       localStorage.setItem("count", count);
+//     }
+//   });
 
 
 // deductions by 5 seconds if incorrect 
