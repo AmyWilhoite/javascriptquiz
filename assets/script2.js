c65e419be0717cbb68e0d54a2e273ed650a5a18c
AmyWilhoite <amywilhoite@gmail.com>
2022-01-28 01:00:09 -0500

    another version of javascript - still working on this homework

--- assets/script2.js
+++ assets/script2.js
@@ -1,5 +1,7 @@
-console.log("this page works!! 2nd Method to achieve this homework!")
-console.log("Im trying to add a method to this constructor function that carries out the function of checking answer then loop through the question - my method may not be best. Im committed!")
+console.log("this page works!! 2nd Method to achieve this homework!");
+console.log(
+  "Im trying to add a method to this constructor function that carries out the function of checking answer then loop through the question - my method may not be best. Im committed!"
+);
 
 // define variables for display on HTML using getElement or querySelector
 var timerEl = document.getElementById("timer");
@@ -10,114 +12,138 @@ var answerEl1 = document.getElementById("btn1");
 var answerEl2 = document.getElementById("btn2");
 var answerEl3 = document.getElementById("btn3");
 var answerEl4 = document.getElementById("btn4");
-var returnAns = document.getElementById ("returnAns");
+var returnAns = document.getElementById("returnAns");
 
-
+var questionsList = [
+  {
+    question: "An API interacts with programs in:",
+    answers: ["browser", "scripts", "platforms", "all of the above"],
+    correctAns: "browser",
+  },
+  {
+    question: "Which of the following is a string?",
+    answers: ["cat", "25", "true or false", "Null"],
+    correctAns: "cat",
+  },
+];
+var currentIndex = 0; //start point
 
 // Tried using the construction function because I want to learn objects better AND
 // I thought this would be cool to use if I had many questions or data to add more easily in the future
-function QuestionFactory (questions, answers, correctAns){
-    this.questions = questions;
-    this.answers = answers;
-    this.correctAns = correctAns;
-// i want to create a method/function to insert here return correct answer for each question automatically if possible 
-// if onclick event for answers [index item] === correctAnswer then + 5 to scoreEl else = =5 seconds to timeEl
-    }
+function QuestionFactory() {
+  // created objects that contain all the data for each question using the construction model
+  // so i could easily create or change out questions in the future
 
+  console.log(questionsList);
+  console.log("Question 1", questionsList[currentIndex].question);
+  console.log("Question 1", questionsList[currentIndex].answers[0]);
+  console.log("Question 1", questionsList[currentIndex].answers[1]);
+  console.log("Question 1", questionsList[currentIndex].answers[3]);
+  console.log("Question 1", questionsList[currentIndex].answers[4]);
+  console.log("Question 1", questionsList[currentIndex].correctAns);
 
-// created objects that contain all the data for each question using the construction model
-// so i could easily create or change out questions in the future
-
-var question1 = new QuestionFactory("An API interacts with programs in:", ["browsers", "scripts", "platforms", "all of the above"], "browser");
-// i  wanted to know if this formula is working
-console.log(question1.questions);
-console.log(question1.answers);
-console.log(question1.correctAns);
-
-// this is how i got display in browser, i'd like to loop through questions once fully developed construction method applies to that section
-questionEl.textContent = "Question 1: "+ question1.questions;
-answerEl1.textContent = question1.answers[0];
-answerEl2.textContent = question1.answers[1];
-answerEl3.textContent = question1.answers[2];
-answerEl4.textContent = question1.answers[3];
-
-var question2 = new QuestionFactory("Which of the following is a string?", ["cat", "25", "true or false", "Null"], "cat");
-console.log(question2.questions);
-console.log(question2.answers);
-console.log(question2.correctAns);
-
-var question3 = new QuestionFactory("Which of the following is a loop:", ["While", "Click", "Return", "All of the above"], "While" );
-console.log(question3.questions);
-console.log(question3.answers);
-console.log(question3.correctAns);
-
+  // this is how i got display in browser, i'd like create a loop through questions once fully developed construction method applies to that section
+  questionEl.textContent = "Question: " + questionsList[currentIndex].question;
+  answerEl1.textContent = questionsList[currentIndex].answers[0];
+  answerEl2.textContent = questionsList[currentIndex].answers[1];
+  answerEl3.textContent = questionsList[currentIndex].answers[2];
+  answerEl4.textContent = questionsList[currentIndex].answers[3];
+  //Adding event listener
+  answerEl1.addEventListener("click", checkAnswer);
+  answerEl2.addEventListener("click", checkAnswer);
+  answerEl3.addEventListener("click", checkAnswer);
+  answerEl4.addEventListener("click", checkAnswer);
+}
 
 // Version 1 function for constructor object attempted: listener for if answer click is correct answer, correct + 5 pts to score otherwise -5 seconds to timer
 // To Do: this sort of works but not the way it's supposed to
 // answerEl.addEventListener("click", function(){
 //     if (this.answers === this.correctAns) {
-//         alert ("love your mom");
+//         alert ("my random text");
 //         scoreEl.textContent +5;
 //     }else {
-//         alert("your bad")
+//         alert("something else here")
 //         timerEl = timerInterval -5;
 //     }
 // })
 
+// TO-DO #1 Add event listener to onclick button not working, then if else reduce time
 // Version 2 function for constructor object attempted: function for listening to click then performing checkedAnser at this correct answer return function
-// add listener to section of HTML defined as answerEl
-answerEl.addEventListener("click", function(event){
-    // add variable for event "click and the target button item"
-    var clickedAns = event.target;
+// add listener to section of HTML container defined element as answerEl
+var storeScore;
+var storeScore = 0;
+var feedbackEL = document.getElementById("feedback");
 
+function checkAnswer() {
+  console.log("button clicked", this.textContent, questionsList[currentIndex].correctAns);
+  if (this.textContent === questionsList[currentIndex].correctAns) {
+    storeScore += 5;
+    (feedbackEL.textContent = "CORRECT"), storeScore, "points!!";
+    //var storeScore = answerEl
+    scoreEl.textContent = storeScore;
+  } else {
+    feedbackEL.textContent =
+      "INCORRECT" + "some function for time to lose 5 seconds";
+    timerEl = secondsLeft - 5;
+  }
+  //move to next question
+  currentIndex++;
+  //call the question factory again
+  QuestionFactory();
+}
+/*
+answerEl.addEventListener("click", function(event){
+// add variable for event "click and the target answer buttons container"
+    var clickedAns = event.answerEl;
+// not sure if i can use the information from the constructor function to do this.
     if (this.answers === this.correctAns) {
-        returnAns.textContent.innerHTML ("CORRECT + 5 points!!");
+        storeScore.textContent.innerHTML ("CORRECT", storeScore + 5, "points!!");
         var storeScore = answerEl
-        scoreEl.textContent +5;
+        scoreEl.textContent(+5);
     }else {
-        returnAns.textContent(innerHTML)("INCORRECT - lose 5 seconds")
-        timerEl = timerInterval -5;
+        returnAns.textContent(innerHTML)("INCORRECT"+ "some function for time to lose 5 seconds")
+        timerEl = secondsLeft - 5;
     }
     // return answer in the proper section
-    clickedAns.returnAns;
+    clickedAns.storeScore;
 });
+*/
 
-
-// To-Do: Function to Loop questions 1,2,3 (constructor function once method developed if possible)
-    // set onclick attribute to all available options looking at this type of option
-    // for(i=0; i < option.length; i++){
-    //     option[i].setAttribute("onclick", "optionSelected(this)");
-
-
-// To Do: Write a formula to score and store user high score but was thinking maybe i can incorporate this in my construction function instead
-// for (var i =0; i < numberofAnswers; i++) {
+// TODO: Score Store: Write a formula to score and store user high score but was thinking maybe i can incorporate this in my construction function instead
+// for (var i =0; i < storeScore; i++) {
 // document.querySelectorAll(".bstyle")[i].addEventListener("click", function(){
-//      if (userInput === correctAns);
-//     // updateScore = userScore + 5
-//     scoreEl.textContent = "test" 
-//     });
+// if (userInput === correctAns);
+// //     // updateScore = userScore + 5
+// scoreEl.textContent = "Score:" + userScore;
+// //     });
+
+// TO-DO: 3: Loop Functions for the type of constructor equations I have if end questions game over
+// Function to Loop questions 1,2,3 (constructor function once method developed if possible)
+// set onclick attribute to all available options looking at this type of option
+// for(i=0; i < option.length; i++){
+//     option[i].setAttribute("onclick", "optionSelected(this)");
 
 // }
 
-// Timer Section - works but no deductions because i dont have my listener correct yet
-// To-do: Timer deducts time when answer is incorrect
+// Timer Section
 
 // start time
 var secondsLeft = 60;
 
 // Timer countdown using setInterval
-var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timerEl.textContent = secondsLeft + " Seconds Remaining";
+var timerInterval = setInterval(function () {
+  secondsLeft--;
+  timerEl.textContent = secondsLeft + " Seconds Remaining";
 
-    if(secondsLeft === 0) {
-      // Stops set interval
-      clearInterval(timerInterval);
-      // message user game over
-      timerEl.textContent = "GAME OVER!"
-    }
+  if (secondsLeft === 0) {
+    // Stops set interval
+    clearInterval(timerInterval);
+    // message user game over
+    timerEl.textContent = "GAME OVER!";
+  }
 }, 1000);
 
+QuestionFactory(); //on pageload
 
 // formula for exploring deductions from the timer with function subtractTime.addEventListener("click", function() {
 //     if (count > 0) {
